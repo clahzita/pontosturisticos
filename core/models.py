6a9a4536a5ec6d8daa69ce6e4c9f2df8a0224c86
clahzita <clah@ufrn.edu.br>
2020-07-27 22:42:06 -0300

    ajeitando erro core/models

--- core/models.py
+++ core/models.py
@@ -18,7 +18,7 @@ class PontoTuristico(models.Model):
     avaliacoes = models.ManyToManyField(Avaliacao)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
     foto = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
-    git add = models.OneToOneField(DocIdentificacao,on_delete=models.CASCADE, null=True, blank=True)
+    doc_identificacao = models.OneToOneField(DocIdentificacao,on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
